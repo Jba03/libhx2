cf241ec31f4bb3e3f40b163c9c732b88c4d73299
Jba03 <jba03@jba03.xyz>
2024-07-16 12:45:59 +0200

    Complete dsp header

--- codec.c
+++ codec.c
@@ -15,13 +15,36 @@
 
 #define DSP_SAMPLES_PER_FRAME 14
 
-struct dsp_adpcm_channel {
-  unsigned int num_samples, remaining;
-  signed short coefficient[16];
-  signed short yn1, yn2, loop_yn1, loop_yn2;
-  unsigned short gain, ps, loop_ps;
-  unsigned int hst1, hst2;
+struct dsp_adpcm {
+  unsigned int num_samples, num_nibbles, sample_rate;
+  unsigned short loop_flag, format;
+  unsigned int loop_start, loop_end, ca;
+  signed short c[16], gain, ps, hst1, hst2;
+  signed short loop_ps, loop_hst1, loop_hst2;
+  /* internal */
+  signed int history1, history2;
+  unsigned int remaining;
 };
+
+static void dsp_adpcm_header_rw(hx_stream_t *s, struct dsp_adpcm *adpcm) {
+  hx_stream_rw32(s, &adpcm->num_samples);
+  hx_stream_rw32(s, &adpcm->num_nibbles);
+  hx_stream_rw32(s, &adpcm->sample_rate);
+  hx_stream_rw16(s, &adpcm->loop_flag);
+  hx_stream_rw16(s, &adpcm->format);
+  hx_stream_rw32(s, &adpcm->loop_start);
+  hx_stream_rw32(s, &adpcm->loop_end);
+  hx_stream_rw32(s, &adpcm->ca);
+  for (int i = 0; i < 16; i++) hx_stream_rw16(s, &adpcm->c[i]);
+  hx_stream_rw16(s, &adpcm->gain);
+  hx_stream_rw16(s, &adpcm->ps);
+  hx_stream_rw16(s, &adpcm->hst1);
+  hx_stream_rw16(s, &adpcm->hst2);
+  hx_stream_rw16(s, &adpcm->loop_ps);
+  hx_stream_rw16(s, &adpcm->loop_hst1);
+  hx_stream_rw16(s, &adpcm->loop_hst2);
+  hx_stream_advance(s, 11 * 2); /* padding */
+}
 
 unsigned int dsp_pcm_size(unsigned int sample_count) {
   unsigned int frames = sample_count / DSP_SAMPLES_PER_FRAME;
@@ -33,20 +56,10 @@ int dsp_decode(hx_t *hx, hx_audio_stream_t *in, hx_audio_stream_t *out) {
   hx_stream_t stream = hx_stream_create(in->data, in->size, HX_STREAM_MODE_READ, in->endianness);
   
   unsigned int total_samples = 0;
-  struct dsp_adpcm_channel channels[in->num_channels];
+  struct dsp_adpcm channels[in->num_channels];
   for (int c = 0; c < in->num_channels; c++) {
-    struct dsp_adpcm_channel* channel = &channels[c];
-    hx_stream_rw32(&stream, &channel->num_samples);
-    hx_stream_advance(&stream, 24);
-    for (int i=0;i<16;i++) hx_stream_rw16(&stream, &channel->coefficient[i]);
-    hx_stream_rw16(&stream, &channel->gain);
-    hx_stream_rw16(&stream, &channel->ps);
-    hx_stream_rw16(&stream, &channel->yn1);
-    hx_stream_rw16(&stream, &channel->yn2);
-    hx_stream_rw16(&stream, &channel->loop_ps);
-    hx_stream_rw16(&stream, &channel->loop_yn1);
-    hx_stream_rw16(&stream, &channel->loop_yn2);
-    hx_stream_advance(&stream, 11 * 2);
+    struct dsp_adpcm* channel = &channels[c];
+    dsp_adpcm_header_rw(&stream, channel);
     total_samples += channels[c].num_samples;
     channels[c].remaining = channels[c].num_samples;
   }
@@ -64,15 +77,15 @@ int dsp_decode(hx_t *hx, hx_audio_stream_t *in, hx_audio_stream_t *out) {
   
   for (int i = 0; i < num_frames; i++) {
     for (int c = 0; c < out->num_channels; c++) {
-      struct dsp_adpcm_channel *adpcm = channels + c;
+      struct dsp_adpcm *adpcm = channels + c;
       const signed char ps = *src++;
       const signed int predictor = (ps >> 4) & 0xF;
       const signed int scale = 1 << (ps & 0xF);
-      const signed short c1 = adpcm->coefficient[predictor * 2 + 0];
-      const signed short c2 = adpcm->coefficient[predictor * 2 + 1];
+      const signed short c1 = adpcm->c[predictor * 2 + 0];
+      const signed short c2 = adpcm->c[predictor * 2 + 1];
       
-      signed int hst1 = adpcm->hst1;
-      signed int hst2 = adpcm->hst2;
+      signed int hst1 = adpcm->history1;
+      signed int hst2 = adpcm->history2;
       signed int count = (adpcm->remaining > DSP_SAMPLES_PER_FRAME) ? DSP_SAMPLES_PER_FRAME : adpcm->remaining;
       
       for (int s = 0; s < count; s++) {
@@ -85,8 +98,8 @@ int dsp_decode(hx_t *hx, hx_audio_stream_t *in, hx_audio_stream_t *out) {
         dst[s * out->num_channels + c] = hst1 = sample;
       }
           
-      adpcm->hst1 = hst1;
-      adpcm->hst2 = hst2;
+      adpcm->history1 = hst1;
+      adpcm->history2 = hst2;
       adpcm->remaining -= count;
     }
     
